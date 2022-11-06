60d596923aea521d47674fb3d7dc08e4c92877cb
Nishiyama13 <nishiyamaeel@gmail.com>
2022-11-06 12:39:51 -0300

    Versão layou responsivo

--- script/script2.js
+++ script/script2.js
@@ -5,3 +5,90 @@ selectorOnClick(){
   alert('Selecionou uma opção!');
   return;
 }*/
+
+function carregarQuizzes() {
+  const promise = axios.get(
+    "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes"
+  );
+  promise.then(console.log);
+  promise.catch(erroCarregamentoQuizzes);
+}
+
+function erroCarregamentoQuizzes(erro) {
+  alert("Erro ao carregar quizzes da API");
+  console.log(erro.response.status);
+}
+/*formato recebido pela API
+
+[
+	{
+		id: 1,
+		title: "Título do quizz",
+		image: "https://http.cat/411.jpg",
+		questions: [
+			{
+				title: "Título da pergunta 1",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			},
+			{
+				title: "Título da pergunta 2",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			},
+			{
+				title: "Título da pergunta 3",
+				color: "#123456",
+				answers: [
+					{
+						text: "Texto da resposta 1",
+						image: "https://http.cat/411.jpg",
+						isCorrectAnswer: true
+					},
+					{
+						text: "Texto da resposta 2",
+						image: "https://http.cat/412.jpg",
+						isCorrectAnswer: false
+					}
+				]
+			}
+		],
+		levels: [
+			{
+				title: "Título do nível 1",
+				image: "https://http.cat/411.jpg",
+				text: "Descrição do nível 1",
+				minValue: 0
+			},
+			{
+				title: "Título do nível 2",
+				image: "https://http.cat/412.jpg",
+				text: "Descrição do nível 2",
+				minValue: 50
+			}
+		]
+	}
+]
+*/
